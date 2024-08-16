e55a5599b63dadf75ec420d762adb2731ef6c0a8
Irvin Yoel <irvinyoel.cerna@gmail.com>
2024-08-16 09:32:05 -0500

    Avance

--- jquerys.js
+++ jquerys.js
@@ -1,11 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const menuOverlay = document.querySelector('.menu-overlay');
-const closeBtn = document.querySelector('.close-btn');
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu");
+  const menuOverlay = document.querySelector(".menu-overlay");
+  const closeBtn = document.querySelector(".closebtn");
 
-hamburger.addEventListener('click', () => {
-    menuOverlay.classList.add('open');
-});
+  menu.addEventListener("click", () => {
+    menuOverlay.classList.add("open");
+  });
 
-closeBtn.addEventListener('click', () => {
-    menuOverlay.classList.remove('open');
+  closeBtn.addEventListener("click", () => {
+    menuOverlay.classList.remove("open");
+  });
 });
