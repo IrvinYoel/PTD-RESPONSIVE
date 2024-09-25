34c2db97327b922b8e831cd81726d922a3421d13
Irvin Yoel <irvinyoel.cerna@gmail.com>
2024-09-24 23:27:44 -0500

    Avance

--- jquerys.js
+++ jquerys.js
@@ -11,3 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
     menuOverlay.classList.remove("open");
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".tablet .img-menu");
+  const menuOverlay = document.querySelector(".tablet .menu-overlay");
+  const closeBtn = document.querySelector(".tablet .closebtn");
+
+  menu.addEventListener("click", () => {
+    menuOverlay.classList.add("open");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    menuOverlay.classList.remove("open");
+  });
+});
